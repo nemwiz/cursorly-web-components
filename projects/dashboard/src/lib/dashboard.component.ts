0bd445c1a57d948703d0bd0a8ad019c18132858d
Nemanja Ninkovic <nemanja.ninkovic@zuehlke.com>
2023-03-24 06:01:43 +0800

    chore: initial project setup

---
+++ projects/dashboard/src/lib/dashboard.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-dashboard',
+  template: `
+    <p>
+      dashboard works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class DashboardComponent {
+
+}
