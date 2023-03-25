b8d7910a502cabab52253f517bf5db9c5ce4424b
Nemanja Ninkovic <nemanja.ninkovic@zuehlke.com>
2023-03-25 10:39:46 +0800

    fix: excludes template for story creation from Storybook

--- packages/web-components/generate-component.sh
+++ packages/web-components/generate-component.sh
@@ -5,4 +5,4 @@ yarn generate $1
 mv src/components/$1/test/$1.spec.tsx src/components/$1/
 rm -rf src/components/$1/test
 
-sed -e "s;%name%;$1;g" -e "s;%NAME%;$2;g" template.stories.mdx > src/components/$1/$1.stories.mdx
+sed -e "s;%name%;$1;g" -e "s;%NAME%;$2;g" template.mdx > src/components/$1/$1.stories.mdx
