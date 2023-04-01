768693612439a7e91bf111d75cb90793c6b0b82f
Nemanja Ninkovic <nemanja.ninkovic@zuehlke.com>
2023-04-01 19:06:19 +0800

    refactor: migrates project to Storybook V7, uses Vite instead of Webpack

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,25 +3,29 @@ module.exports = {
         '../packages/**/*.stories.mdx',
         '../packages/**/*.stories.@(js|jsx|ts|tsx)'
     ],
-    addons: [
-        '@storybook/addon-links',
-        '@storybook/addon-essentials',
-        {
-            name: '@storybook/addon-docs',
-            options: {
-                transcludeMarkdown: true
-            }
-        },
+    addons: ['@storybook/addon-links', '@storybook/addon-essentials', {
+        name: '@storybook/addon-docs',
+        options: {
+            transcludeMarkdown: true
+        }
+    },
         'storybook-addon-themes',
         '@storybook/addon-interactions',
+        '@storybook/addon-mdx-gfm'
     ],
     features: {
         buildStoriesJson: true,
         interactionsDebugger: true,
-        // storyStoreV7: true
+        storyStoreV7: true
     },
-    framework: '@storybook/html',
+    framework: {
+        name: '@storybook/html-vite',
+        options: {}
+    },
+    docs: {
+        autodocs: true
+    },
     core: {
-        builder: 'webpack5'
+        builder: '@storybook/builder-vite'
     }
-}
+};
